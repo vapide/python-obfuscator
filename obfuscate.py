be842d8bf9254546726d68fb65a409fb43bc293a
vapide <93092420+vapide@users.noreply.github.com>
2022-01-24 18:24:48 -0700

    added obfuscatefile function
    added the obfuscatefile function for the new interface I made

--- obfuscate.py
+++ obfuscate.py
@@ -68,20 +68,26 @@ def encrypt(code):
 def decrypt(code, key):
     return unicodetostring(decompress(code, key))
 
+def readfile(path):
+	with open(path, 'r') as file:
+		return file.read()
+
 def obfuscatecode(code):
 	oldcode = code
 	encryptedcode = encrypt(code)
-	buffer = 'O=dict;r=zip;t=chr;T=\'raise\';U=\'Exception\';s=range;n=set;w=len;X=isinstance;n=eval;F=\'*args\';g=tuple;W=[\'0x000070\',\'0x000072\',\'0x000069\',\'0x00006e\',\'0x000074\'];import math,pathlib,os,ast,sys;H=math.log;u=math.ceil;P=ast.literal_eval;L=[None,False,__name__,exec,True,int(0),2,3,\'hi\',\'exec\'];J=O(r([t(i)for i in s(1114111)],[\"{0:#0{1}x}\".format(i,8)for i in s(1114111)],));q=O((v,k)for k,v in J.items());z=[\'0x000065\',\'0x000078\',\'0x000065\',\'0x000063\'];V=T+q[\'0x000020\']+U;\ndef Z(l,L):\n for k in n(L):l+=q[k];\n return l\ndef C(f):\n bc=\"/\"\n for F in f:bc+=J[F]+\"/\";\n return bc\ndef l(bc):\n f=\"\";bc=bc.split(\"/\");bc=\" \".join(bc).split();\n for v in bc:f+=q[v];\n return f\ndef N(dictionary):\n G=[]\n for U in dictionary:G.append((U,dictionary[U]));\n G.sort(key=lambda x:x[1]);G.reverse();return G\ndef j(f):\n return{i:f.count(i)for i in n(f)}\ndef E(frequencies):\n return u(H(w(frequencies),16))\ndef B(G,hexlim):\n p={}\n for i in s(w(G)):p[G[i][0]]=\"{0:#0{1}x}\".format(i,hexlim+2)+\"/\";\n return p\ndef y(f,W):\n if W and f:s,r=W.popitem();return r.join(y(subs,O(W))for subs in f.split(s));\n return f\ndef D(f):\n return y(f,B(N(j(f)),E(j(f)))),B(N(j(f)),E(j(f)))\ndef c(f,keys=\"\"):\n if X(f,g):keys=f[1];f=f[0];\n W=O((v,k)for k,v in keys.items());return y(f,W)\ndef T(code):\n I=code;A=D(C(code))[1];Y=D(C(code))[0];return I,A,Y\ndef M(code,A):\n return l(c(code,A))\n#n(Z(\'\',\'z\'))(Z(\'\',\'W\') + \'=\' + V)\nn(Z(\'\',\'z\'))(\'def \'+Z(\'\',\'W\')+\'(\'+F+\'): \'+V+\'(\"\'+n(\'t(110)+t(111)+t(112)+t(101)+t(33)+t(34)+t(41)+t(59)\'))\n'
+	buffer = 'O=dict;r=zip;t=chr;T=\'raise\';U=\'Exception\';s=range;n=set;w=len;X=isinstance;n=eval;F=\'*args\';g=tuple;W=[\'0x000070\',\'0x000072\',\'0x000069\',\'0x00006e\',\'0x000074\'];import math,pathlib,os,ast,sys;H=math.log;u=math.ceil;P=ast.literal_eval;L=[None,False,__name__,exec,True,int(0),2,3,\'hi\',\'exec\'];J=O(r([t(i)for i in s(1114111)],[\"{0:#0{1}x}\".format(i,8)for i in s(1114111)],));q=O((v,k)for k,v in J.items());z=[\'0x000065\',\'0x000078\',\'0x000065\',\'0x000063\'];V=T+q[\'0x000020\']+U;\ndef Z(l,L):\n for k in n(L):l+=q[k];\n return l\ndef C(f):\n bc=\"/\"\n for F in f:bc+=J[F]+\"/\";\n return bc\ndef l(bc):\n f=\"\";bc=bc.split(\"/\");bc=\" \".join(bc).split();\n for v in bc:f+=q[v];\n return f\ndef N(dictionary):\n G=[]\n for U in dictionary:G.append((U,dictionary[U]));\n G.sort(key=lambda x:x[1]);G.reverse();return G\ndef j(f):\n return{i:f.count(i)for i in n(f)}\ndef E(frequencies):\n return u(H(w(frequencies),16))\ndef B(G,hexlim):\n p={}\n for i in s(w(G)):p[G[i][0]]=\"{0:#0{1}x}\".format(i,hexlim+2)+\"/\";\n return p\ndef y(f,W):\n if W and f:s,r=W.popitem();return r.join(y(subs,O(W))for subs in f.split(s));\n return f\ndef D(f):\n return y(f,B(N(j(f)),E(j(f)))),B(N(j(f)),E(j(f)))\ndef c(f,keys=\"\"):\n if X(f,g):keys=f[1];f=f[0];\n W=O((v,k)for k,v in keys.items());return y(f,W)\ndef T(code):\n I=code;A=D(C(code))[1];Y=D(C(code))[0];return I,A,Y\ndef M(code,A):\n return l(c(code,A))\n#n(Z(\'\',\'z\'))(Z(\'\',\'W\') + \'=\' + V)\n#n(Z(\'\',\'z\'))(\'def \'+Z(\'\',\'W\')+\'(\'+F+\'): \'+V+\'(\"\'+n(\'t(110)+t(111)+t(112)+t(101)+t(33)+t(34)+t(41)+t(59)\'))\n'
 	newcode = buffer + "n(Z('','z'))(M('" + encryptedcode[1] + "',P(l(\'" + stringtounicode(str(encryptedcode[2])) + '\'))))'
 	return newcode
 
-def obfuscate(name, code, compile = False, clearvars = False):
-	code = code.replace('print', 'sys.stdout.write')
+def obfuscate(name, code, compile = False, clearvars = False, outputname = None):
+	if outputname == None:
+		outputname = name + '.py'
+	#code = code.replace('print', 'sys.stdout.write')
 	if clearvars == True:
 		code += '\nsys.modules[__name__].__dict__.clear()' # removes the variables from locals() globals() and vars()
 	code = obfuscatecode(code)
-	filepath = str(pathlib.Path(str(pathlib.Path(__file__).parent)+'/temp/'+name+'.py')) # path to the obfuscated file (before compiling)
-	outputpath = str(pathlib.Path(str(pathlib.Path(__file__).parent)+'/output/'+name+'.py')) # path to the final obfuscated file (after compiling)
+	filepath = str(pathlib.Path(str(pathlib.Path(__file__).parent)+'/temp/'+outputname)) # path to the obfuscated file (before compiling)
+	outputpath = str(pathlib.Path(str(pathlib.Path(__file__).parent)+'/output/'+outputname)) # path to the final obfuscated file (after compiling)
 	if compile == True:
 		if not os.path.exists(os.path.dirname(filepath)):
 			try:
@@ -105,3 +111,6 @@ def obfuscate(name, code, compile = False, clearvars = False):
 			f.write(code)
 		print(outputpath)
 	return outputpath
+
+def obfuscatefile(path):
+	obfuscate(os.path.basename(path), readfile(path), compile = True, clearvars = True, outputname = os.path.basename(path))
